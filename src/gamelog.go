a44c775bb69dbc374823ce7af011d729f6ddecc1
Lukas Veneziano <fs@venezilu.de>
2020-08-25 21:44:32 +0200

    Reformat timestamps in factorio log

--- src/gamelog.go
+++ src/gamelog.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"log"
+    "errors"
+    "log"
+    "regexp"
+    "strings"
+    "time"
 
-	"github.com/hpcloud/tail"
+    "github.com/hpcloud/tail"
 )
+
 
 func tailLog(filename string) ([]string, error) {
 	result := []string{}
@@ -19,5 +24,58 @@ func tailLog(filename string) ([]string, error) {
 		result = append(result, line.Text)
 	}
 
-	return result, nil
+  result = reformatTimestamps(result)
+
+  return result, nil
+}
+
+
+func getOffset(line string) (string, error) {
+  re, _ := regexp.Compile(`^\d+.\d+`)
+
+  if !re.MatchString(line) {
+    log.Printf("This line has no offset", line)
+    return "error", errors.New(line)
+  }
+
+  offset := re.FindString(line)
+
+  return offset, nil
+}
+
+
+func getStartTime(line string) (time.Time) {
+  re, _ := regexp.Compile(`\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}`)
+  date := string(re.FindString(line))
+  startTime, _ := time.Parse(time.RFC3339, strings.Replace(date, " ", "T", 1) + "Z")
+
+  return startTime
+}
+
+
+func replaceTimestampInLine(line string, offset string, startTime time.Time) (string) {
+    offset, err := getOffset(line)
+    offsetDuration, _ := time.ParseDuration(offset + "s")
+    timestamp := startTime.Add(offsetDuration)
+
+    if err == nil  {
+      return timestamp.Format("2006-01-02 03:04:05") +  ":" + strings.Replace(line, offset, "", 1)
+    } else {
+      return line
+    }
+}
+
+
+func reformatTimestamps(log []string) ([]string) {
+  firstLine := log[len(log)-1]
+  startTime := getStartTime(firstLine)
+  result := []string{}
+
+  for i := range log {
+    line := log[i]
+    offset, _ := getOffset(line)
+    result = append(result, replaceTimestampInLine(line, offset, startTime))
+  }
+
+  return result
 }
